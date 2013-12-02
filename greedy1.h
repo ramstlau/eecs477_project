e9255dac67ddc9a07abe8a29690f4a19da3d729e
Laura Ramstad <ramstlau@umich.edu>
2013-12-02 16:19:00 -0500

    adding short documentation for greedy1 and greedy2

--- greedy1.h
+++ greedy1.h
@@ -1,6 +1,13 @@
 #ifndef _GREEDY1_H_
 #define _GREEDY1_H_
 
+//Pseudo Code:
+//  1. Sort antennas based on cardinality of base stations it covers (high -> low)
+//  2. Select the highest one
+//  3. Walk through sorted range from step one, adding an antenna if it is profitable:
+//	Profitable: calculate for this antenna |# of base stations in this that don't receive signal upon adding this|/|# of total base stations that this covers|.
+//		    Adding this is a profit if the division above is below 0.5. 
+
 bool profitable(HittingSetData &data, int setNum, vector<int> &covered, vector<int> &onecovered, vector<int> &dc);
 double profitable_calculate(HittingSetData &data, int setNum, vector<int> &covered, vector<int> &onecovered, vector<int> &dc, vector<int> &existingdc, vector<int> &adddc);
 void profitable_add(HittingSetData &data, int setNum, vector<int> &covered, vector<int> &onecovered, vector<int> &dc, vector<int> &existingdc, vector<int> &adddc);
