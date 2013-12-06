7019ce5c9a8db45d81b0f429db2c49214e6bc6bc
Laura Ramstad <ramstlau@umich.edu>
2013-12-06 12:20:01 -0500

    cleaning up code

--- validator.cpp
+++ validator.cpp
@@ -15,11 +15,9 @@ void validate(HittingSetData &data, vector<int> &set_antennas, unsigned int &num
 	for(unsigned int i = 0; i < set_antennas.size(); i++)
 	  {
 	    int antenna = set_antennas.at(i);
-	    //cout << "antenna : " << antenna << " : ";
 	    for(unsigned int j = 0; j < data.antennas.at(antenna).base_stations.size(); j++)
 	      {
 	        int bs = data.antennas.at(antenna).base_stations.at(j);
-	        //cout << bs << " ";
 	        if(bs_coverage_freq.find(bs) == bs_coverage_freq.end())
 	          {
 	            bs_coverage_freq[bs] = 1;
@@ -33,27 +31,13 @@ void validate(HittingSetData &data, vector<int> &set_antennas, unsigned int &num
 	          }
 	      
 	      }
-	  //cout << endl;
+
 	  }
-/*
-	for(unordered_map<int, int>::iterator it = bs_coverage_freq.begin(); it != bs_coverage_freq.end(); it++)
-	  cout << "base station: " << it->first << " has freq : "<< it->second << endl;
-	cout << endl;
-	//cout << "printing oc in validator : ";
-	for(unordered_set<int>::iterator it = onecovered_bs.begin(); it != onecovered_bs.end(); it++)
-	  {
-	    //cout << *it << " ";
-	    oc.push_back(*it); 
-	  }
-	//cout << endl;
-	
-	sort(oc.begin(), oc.end());
-	//for(unordered_set<int>::iterator it = onecovered_bs.begin(); it != onecovered_bs.end(); it++)*/
+
 	  
   if(num_covered_base_stations != onecovered_bs.size())
     throw (unsigned int)onecovered_bs.size();
 
-	//return oc;
 
 
 }
